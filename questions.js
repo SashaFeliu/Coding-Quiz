294bc17054623bdb1c0d52efc406d3f89657acef
SashaFeliu <58046474+SashaFeliu@users.noreply.github.com>
2020-04-25 23:39:45 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,52 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "The basic difference between JavaScript and Java is _________",
+    choices: [
+      "There is no difference",
+      "Functions are considered as fields",
+      "Variables are specific",
+      "Functions are values, and there is no hard distinction between methods and fields"
+    ],
+    answer:
+      "Functions are values, and there is no hard distinction between methods and fields"
+  },
+  {
+    title:
+      "What are the three important manipulations done in a for loop on a loop variable?",
+    choices: [
+      "Updation, Incrementation, Initialization",
+      "Initialization,Testing, Updation",
+      "Testing, Updation, Testing",
+      "Initialization,Testing, Incrementation"
+    ],
+    answer: "Initialization,Testing, Updation"
+  },
+
+  {
+    title: "What is the function used to remove all handlers for name events",
+    choices: [
+      "deleteAllListeners(name)",
+      "deleteListener(name,f)",
+      "removeListener(name,f)",
+      "removeAllListeners(name)"
+    ],
+    answer: "removeAllListeners(name)"
+  },
+  {
+    title:
+      "What will be the return value of the write() method when the Node cannot write the data immediately and has to buffer it internally?",
+    choices: ["0", "1", "True", "False"],
+    answer: "False"
+  },
+  {
+    title: "What is the purpose of the basic validation?",
+    choices: [
+      "Data correctness",
+      "Mere data existence",
+      "Both Data correctness and Mere data existence",
+      "Data modification"
+    ],
+    answer: "Mere data existence"
+  }
+];
